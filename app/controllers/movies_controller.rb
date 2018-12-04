e3a9844b40cfd4533e0cf29557afc69b25d2a1d5
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2018-12-04 17:31:28 +0100

    Correct routes.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
   returns :array_of => :movie, :code => 200, :desc => "All movies"
   def index
     @movies = apply_scopes(Movie.all)
-    render 'movies/index', locals: {movies: @movies}
+    render @movies
   end
 
   api :GET, "movies/:id", "Get specific movie"
