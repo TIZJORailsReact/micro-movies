a7b485180881c5e6289691c588380d826344791a
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2018-12-04 17:52:34 +0100

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   apipie
 
-    resources :movies, only: %i(index show), :defaults => { :format => 'json' } do
-      resources :comments, except: :show, :defaults => { :format => 'json' }
+    resources :movies, only: %i(index show) do
+      resources :comments, except: :show
     end
 end
