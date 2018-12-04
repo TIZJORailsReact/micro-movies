59e6b0acaeda8566681dd15e3d680f76903ba505
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2018-12-04 10:44:07 +0100

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create!(title: "Captain Marvel", genre: :action, link: "tkSuLa3o_5A")
+Movie.create!(title: "Kung Fury", genre: :action, link: "bS5P_LAqiVg")
+Movie.create!(title: "Operation Overlord", genre: :horror, link: "kQBCQpl4VMI")
+Movie.create!(title: "Zabójcze maszyny", genre: :action, link: "5utVQRSIAYs")
+Movie.create!(title: "The Lion King", genre: :animation, link: "4CbLXeGSDxg")
+Movie.create!(title: "Aquaman", genre: :action, link: "h-k0-Z9b4Cw")
+Movie.create!(title: "X-MEN Dark Phoenix", genre: :action, link: "QvpYp11Q7fA")
+Movie.create!(title: "Bumblebee", genre: :action, link: "lcwmDAYt22k")
+Movie.create!(title: "Robin Hood", genre: :action, link: "NXfjRgc8Dls")
